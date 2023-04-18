4f41a29dc694160a9686bc0f8331b5db42fd400c
dale <hessdale96@gmail.com>
2023-04-18 16:29:03 -0600

    warming up with vue

---
+++ src/components/FunComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="change_back">Change Background</button>
+    <section v-for="(secret, i) in secrets" :key="i">
+      <p @click="change_pink">{{ secret }}</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    change_back: function () {
+      document.body[`style`][`background`] = `blue`;
+    },
+    change_pink: function () {
+      document.querySelector(`p`)[`style`][`color`] = `pink`;
+    },
+  },
+  data() {
+    return {
+      secrets: [`secret1`, `secret2`, `secret3`, `secret4`, `secret5`],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
